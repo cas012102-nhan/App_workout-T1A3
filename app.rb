0838ee1608d0509d7d942612feb3d8fff8d253d1
nhan <=nhan@coderacademy.edu.au>
2021-04-06 14:05:04 +1000

    redo

--- app.rb
+++ app.rb
@@ -2,52 +2,6 @@ require 'tty-prompt'
 require 'colorize'
 
 
-puts "Welcome to Fitness App".red
-puts ".........."
-prompt = TTY::Prompt.new
-prompt.ask("What is your name?")
-puts ""
-prompt.ask("How old are you?")
-puts ""
-prompt.yes?("Do you want to check BMI?")
-puts ""
-
-class Bmi
-    attr_reader :weight, :height
-    def initialize(weight, height)
-        @weight = weight
-        @height= height
-    end
-
-    def add(weight, height)
-        p "Enter your weight: #{@weight} kgs"
-        p "Enter your height: #{@height} meters"
-    end
-
-    def get_bmi
-        p " Your BMI is: "
-        @weight / (@height * @height)
-    end
-
-end
-bmi = Bmi.new(80, 1.7)
-bmi.add(80,1.7)
-p bmi.get_bmi 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,22 +1,4 @@
-require_relative '../app'
-require_relative '../bmi'
-describe Bmi do
-    it 'should return BMI' do
-        weight = 60
-        height = 1.65
-        bmi = Bmi.new(weight, height)
-        bmi.add(weight, height)
-        expect(bmi.get_bmi).to eq(weight/(height*height))
 
-    end
 
-    it'should be able to return  weight and height' do
-        weight = weight
-        height = height
-        info = Bmi.new(weight, height)
-        info.add(weight, height)
-        expect(info.weight).to eq(weight)
-        expect(info.height).to eq(height)  
-    end
-end
+
 
