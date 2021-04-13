3da8a64ae167f4d80a25461fe0459d4374f435a7
nhan <=nhan@coderacademy.edu.au>
2021-04-13 17:28:15 +1000

    added rspec

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,5 +1,7 @@
-
-
-
-
-
+require_relative '../app'
+require "rspec"
+describe Game do
+    it 'should be able to return workout plans with difficulty level' do
+        
+    end
+end
--- spec/spec1/app_spec1.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../module'
-require 'rspec'
-describe 'Game' do
-    it 'should return diffrent workout plans' do
-    game = Game.new
-    b = game.training(difficulty, diets)
-    expect(game.training).to be eq(training)
-    end
-end
